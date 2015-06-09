fa8a94cb13d00531dcd8acb81adf6e9e6a70217b
Guilherme Severo <grsevero@gmail.com>
2015-06-09 17:41:46 -0300

    adicionado login

--- app/index.js
+++ app/index.js
@@ -2,8 +2,15 @@ var express = require('express'),
     querystring = require('querystring'),
     config = require('./config'),
     cookieParser = require('cookie-parser'),
-    expressLayouts = require('express-ejs-layouts');
-    var bodyParser = require('body-parser');
+    expressLayouts = require('express-ejs-layouts'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    expressSession = require('express-session'),
+    passport = require('passport'),
+    MongoStore = require('connect-mongo')(expressSession),
+    bodyParser = require('body-parser');
+
+//mongoose.connect(process.env.MONGOHQ_URL || config.db.mongo_url);
 
 var app = express();
 
@@ -14,11 +21,23 @@ app.set('layout', 'layouts/index'); // defaults to 'layout'
 app.set("layout extractScripts", true);
 
 app.use(expressLayouts);
-app.use(bodyParser.json());
 app.use(express.static(config.public_dir));
-app.use(require('./routes.js'));
-
-//app.use(require('body-parser'));
+app.use(expressSession({
+    secret: config.cookie.secret,
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection
+    })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser(config.cookie.secret));
 
+
+
+//Colocar por ultimo
+app.use(require('./routes.js'));
 module.exports = app;
--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,40 @@
 var express = require('express')
 var router = express.Router()
 var controllers = require('./controllers')
+var passport = require('passport')
+var huia_auth = require('./services/huia_auth')
+var LocalStrategy = require('passport-local').Strategy
+
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        var user = huia_auth.auth(username, password, function(err, user) {
+            return done(null, user)
+        })
+    }
+))
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+router.get('/login', controllers.login.get_login)
+router.post('/login',
+    passport.authenticate('local', {
+        failureRedirect: '/login',
+        successRedirect: '/'
+    }),
+    function(req, res) {
+        res.redirect('/')
+    })
+
 
 //Home
-router.get('/', controllers.instagram.get_index)
+router.get('/', authorize, controllers.instagram.get_index)
 
 //Facebook
 router.get('/facebook/challenge', controllers.facebook.get_challenge)
@@ -16,3 +47,9 @@ router.post('/instagram/subscribe', controllers.instagram.post_subscribe)
 router.get('/instagram/photos', controllers.instagram.get_photos)
 
 module.exports = router
+
+
+function authorize(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
